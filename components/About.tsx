c34924e75fe33ef67e26df73f275ad3fc8d60263
toshi-Devs <trahim87mustapha@gmail.com>
2024-02-07 23:36:33 +0200

    just coloring cs

--- components/About.tsx
+++ components/About.tsx
@@ -11,7 +11,7 @@ function About() {
                     Credit my two brothers for sparking my coding journey <span className=' text-[#40CF8E]'>– their passion was contagious. </span>
                     While I haven't conquered full-stack development yet, I'm on the grind, soaking up knowledge like a sponge.
                     There's something thrilling about diving into the unknown, and coding feeds that craving for discovery.
-                    Oh, and when I'm not tinkering with code, you'll find me in the virtual battlegrounds of Counter Strike, 
+                    Oh, and when I'm not tinkering with code, you'll find me in the virtual battlegrounds of <span className='text-[#E58716]'>Counter Strike</span>, 
                     where I trade pixels for headshots. Life's an adventure, and I'm here for the ride!</p>
             </div>  
             <div className='space-y-3 py-10 mx-10'>
