99830c28cce9956fbe7c81ab9badd5cc5582b64f
toshi-Devs <trahim87mustapha@gmail.com>
2024-02-10 13:10:28 +0200

    Projects part done

--- components/Header.tsx
+++ components/Header.tsx
@@ -33,7 +33,7 @@ function Header() {
                 <Link href="#" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Logo</Link>
                 <Link href="#" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Home</Link>
                 <Link href="#About" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> About</Link>
-                <Link href="#" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Projects</Link>
+                <Link href="#Projects" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Projects</Link>
                 <Link href="#" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Contact</Link>
                 </div> :
               <div className='py-2 px-3 rounded-xl select-none text-slate-400 font-bold'>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,13 +4,13 @@ import { FaGithub } from 'react-icons/fa6'
 
 function Projects() {
   return (
-    <div className='flex flex-col min-h-screen bg-slate-950 border-t-lime-200 border-t-2 py-10'>
+    <div className='flex flex-col min-h-screen bg-slate-950 border-t-lime-200 border-t-2 py-10' id='Projects'>
         
         <h2 className='text-2xl font-bold text-slate-100 mx-10 pb-2'>Projects</h2>
            
                     <div className='grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-2'>
                         
-                        <div className='bg-[#1C1C1C] m-3 border-2 border-lime-100 shadow-lg backdrop-blur-md p-5'>
+                        <div className='bg-[#1C1C1C] m-3 border-2 border-lime-100 rounded-sm shadow-lg backdrop-blur-md p-5'>
                          {/* Now i will put a link to code and live website on top right of this div */}
                             <div className='flex flex-row justify-end space-x-3 pt-2'>
                                 <a href='https://github.com/toshi-Devs/Dropbox-clone-nextjs' target='_blank' 
@@ -19,19 +19,19 @@ function Projects() {
                                 className='text-slate-100 hover:text-lime-200 transition-all duration-300 flex flex-col items-center'><FaLink size={25} /> </a>
                             </div>
 
-                            <div className='flex flex-col items-center justify-center space-y-5 pb-5'>
+                            <div className='flex flex-col items-center justify-center space-y-5 pb-5 cursor-pointer'>
                                 <h3 className='text-xl font-bold font-sans text-slate-100'>Dropbox Clone</h3>
                                 <p className='text-lg text-slate-400 font-medium text-center'>Dropbox clone allows you upload and save you files using Typescript and NextJs</p>
                                 <div className='flex flex-row space-x-1 gap-1 items-center uppercase font-mono'>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#FBD38D] border-[#FBD38D] bg-[#fbd38d29]'> NEXTJS</p>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#9DECF9] border-[#9DECF9] bg-[#fbd38d29]'> Typescript</p>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#FBB6CE] border-[#FBB6CE] bg-[#fbd38d29]'> TailwindCSS</p>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#FAF089] border-[#FAF089] bg-[#fbd38d29]'> Firebase</p>                                    
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FBD38D] border-[#FBD38D] bg-[#fbd38d29]'> NEXTJS</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#9DECF9] border-[#9DECF9] bg-[#fbd38d29]'> Typescript</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FBB6CE] border-[#FBB6CE] bg-[#fbd38d29]'> TailwindCSS</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FAF089] border-[#FAF089] bg-[#fbd38d29]'> Firebase</p>                                    
                                 </div>  
                             </div>                          
                         </div>
                         
-                        <div className='bg-[#1C1C1C] m-3 border-2 border-lime-100 shadow-lg backdrop-blur-md p-5'>
+                        <div className='bg-[#1C1C1C] m-3 border-2 border-lime-100 rounded-sm shadow-lg backdrop-blur-md p-5'>
                          {/* Now i will put a link to code and live website on top right of this div */}
                             <div className='flex flex-row justify-end space-x-3 pt-2'>
                                 <a href='https://github.com/toshi-Devs/auth-application' target='_blank'
@@ -40,20 +40,20 @@ function Projects() {
                                 className='text-slate-100 hover:text-lime-200 transition-all duration-300 flex flex-col items-center'><FaLink size={25} /> </a>
                             </div>
 
-                            <div className='flex flex-col items-center justify-center space-y-5 pb-5'>
+                            <div className='flex flex-col items-center justify-center space-y-5 pb-5 cursor-pointer'>
                                 <h3 className='text-xl font-bold font-sans text-slate-100'>Auth App</h3>
                                 <p className='text-lg text-slate-400 font-medium'>A simple authentication app with React and Firebase</p>
                                 <div className='flex flex-row space-x-1 gap-1 items-center uppercase font-mono'>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#FBD38D] border-[#FBD38D] bg-[#fbd38d29]'> Javascript</p>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#D6BCFA] border-[#D6BCFA] bg-[#fbd38d29]'> React</p>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#FBB6CE] border-[#FBB6CE] bg-[#fbd38d29]'> CSS</p>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#FAF089] border-[#FAF089] bg-[#fbd38d29]'> Firebase</p>  
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FBD38D] border-[#FBD38D] bg-[#fbd38d29]'> Javascript</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#D6BCFA] border-[#D6BCFA] bg-[#fbd38d29]'> React</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FBB6CE] border-[#FBB6CE] bg-[#fbd38d29]'> CSS</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FAF089] border-[#FAF089] bg-[#fbd38d29]'> Firebase</p>  
                                 </div>  
                             </div>
                           
                         </div>
 
-                        <div className='bg-[#1C1C1C] m-3 border-2 border-lime-100 shadow-lg backdrop-blur-md p-5'>
+                        <div className='bg-[#1C1C1C] m-3 border-2 border-lime-100 rounded-sm shadow-lg backdrop-blur-md p-5'>
                          {/* Now i will put a link to code and live website on top right of this div */}
                             <div className='flex flex-row justify-end space-x-3 pt-2'>
                                 <a href='https://github.com/toshi-Devs/advice-generator-app' target='_blank'
@@ -62,19 +62,19 @@ function Projects() {
                                 className='text-slate-100 hover:text-lime-200 transition-all duration-300 flex flex-col items-center'><FaLink size={25} /> </a>
                             </div>
 
-                            <div className='flex flex-col items-center justify-center space-y-5 pb-5'>
+                            <div className='flex flex-col items-center justify-center space-y-5 pb-5 cursor-pointer'>
                                 <h3 className='text-xl font-bold font-sans text-slate-100'>Advice Gennerator App</h3>
                                 <p className='text-lg text-slate-400 font-medium text-center'>A very basic advice generator app calling an API for data, Practicing fetching data from APIs</p>
                                 <div className='flex flex-row space-x-1 gap-1 items-center uppercase font-mono'>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#FBD38D] border-[#FBD38D] bg-[#fbd38d29]'> Javascript</p>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#D6BCFA] border-[#D6BCFA] bg-[#fbd38d29]'> React</p>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#FBB6CE] border-[#FBB6CE] bg-[#fbd38d29]'> CSS</p>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#FAF089] border-[#FAF089] bg-[#fbd38d29]'> API</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FBD38D] border-[#FBD38D] bg-[#fbd38d29]'> Javascript</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#D6BCFA] border-[#D6BCFA] bg-[#fbd38d29]'> React</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FBB6CE] border-[#FBB6CE] bg-[#fbd38d29]'> CSS</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FAF089] border-[#FAF089] bg-[#fbd38d29]'> API</p>
                                 </div>  
                             </div>                          
                         </div>
 
-                        <div className='bg-[#1C1C1C] m-3 border-2 border-lime-100 shadow-lg backdrop-blur-md p-5'>
+                        <div className='bg-[#1C1C1C] m-3 border-2 border-lime-100 rounded-sm shadow-lg backdrop-blur-md p-5'>
                          {/* Now i will put a link to code and live website on top right of this div */}
                             <div className='flex flex-row justify-end space-x-3 pt-2'>
                                 <a href='https://github.com/toshi-Devs/Dropbox-clone-nextjs' target='_blank' 
@@ -83,14 +83,14 @@ function Projects() {
                                 className='text-slate-100 hover:text-lime-200 transition-all duration-300 flex flex-col items-center'><FaLink size={25} /> </a>
                             </div>
 
-                            <div className='flex flex-col items-center justify-center space-y-5 pb-5'>
+                            <div className='flex flex-col items-center justify-center space-y-5 pb-5 cursor-pointer'>
                                 <h3 className='text-xl font-bold font-sans text-slate-100'>Dropbox Clone</h3>
                                 <p className='text-lg text-slate-400 font-medium text-center'>Dropbox clone allows you upload and save you files using Typescript and NextJs</p>
                                 <div className='flex flex-row space-x-1 gap-1 items-center uppercase font-mono'>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#FBD38D] border-[#FBD38D] bg-[#fbd38d29]'> NEXTJS</p>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#9DECF9] border-[#9DECF9] bg-[#fbd38d29]'> Typescript</p>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#FBB6CE] border-[#FBB6CE] bg-[#fbd38d29]'> TailwindCSS</p>
-                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-md text-[#FAF089] border-[#FAF089] bg-[#fbd38d29]'> Firebase</p>                                    
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FBD38D] border-[#FBD38D] bg-[#fbd38d29]'> NEXTJS</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#9DECF9] border-[#9DECF9] bg-[#fbd38d29]'> Typescript</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FBB6CE] border-[#FBB6CE] bg-[#fbd38d29]'> TailwindCSS</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FAF089] border-[#FAF089] bg-[#fbd38d29]'> Firebase</p>                                    
                                 </div>  
                             </div>                          
                         </div>
