5a2fa5809b0f53d3de92fbc5bf71ad37635c0077
toshi-Devs <trahim87mustapha@gmail.com>
2024-02-12 17:42:52 +0200

    header done with logo img

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -117,8 +117,11 @@ function Contact() {
                 className='text-slate-100 hover:text-[#40CF8E] transition-all duration-300 flex flex-col items-center hover:scale-110'><FaGithub size={40} /></a>
                 <a href='mailto:trahim87mustapha@gmail.com' target='_blank' 
                 className='text-slate-100 hover:text-[#40CF8E] transition-all duration-300 flex flex-col items-center hover:scale-110'><LuMail size={40} /></a>
+
+                
             </div>
-        
+            <p className='text-lg text-slate-100 font-semibold text-center py-3 m-2 font-mono'>Designed and Coded by 
+            <a href='https://github.com/toshi-Devs' target='_blank' className='text-[#40CF8E] hover:text-lime-400 transition-all duration-300'> Toshi-Devs</a></p>
         </div>
     </div>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Link from 'next/link'
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 
 function Header() {
@@ -29,19 +30,29 @@ function Header() {
     <div className='fixed flex'>
 
         <nav className='fixed flex w-full top-0 left-0 justify-center items-center mt-5 '>
-          {scrolling ? <div className=' bg-[#72727233] backdrop-blur-md shadow-md py-2 px-3 rounded-xl select-none text-slate-400 font-bold'>
-                <Link href="#" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Logo</Link>
-                <Link href="#" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Home</Link>
+          {scrolling ? 
+            <div className=' bg-[#72727233] backdrop-blur-md shadow-md py-2 px-3 rounded-xl select-none text-slate-400 font-bold flex items-center'>
+              {/* put the image next to the navbar items*/}
+              <div className='rounded-full overflow-hidden cursor-pointer'>
+                <Link href='/'>
+                    <Image src='/toshi-logo-nobg.png' alt='logo' width={40} height={40} />
+                </Link>
+              </div>
+                <Link href="/" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Home</Link>
                 <Link href="#About" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> About</Link>
                 <Link href="#Projects" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Projects</Link>
-                <Link href="#" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Contact</Link>
-                </div> :
-              <div className='py-2 px-3 rounded-xl select-none text-slate-400 font-bold'>
-                <Link href="#" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Logo</Link>
-                <Link href="#" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Home</Link>
-                <Link href="#About" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> About</Link>
-                <Link href="#" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Projects</Link>
-                <Link href="#" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Contact</Link>
+                <Link href="#Contact" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Contact</Link>
+            </div> :
+              <div className='py-2 px-3 rounded-xl select-none text-slate-400 font-bold flex items-center'>
+                <div className='rounded-full overflow-hidden cursor-pointer'>
+                <Link href='/'>
+                    <Image src='/toshi-logo-nobg.png' alt='logo' width={40} height={40} />
+                </Link>
+              </div>
+                  <Link href="/" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Home</Link>
+                  <Link href="#About" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> About</Link>
+                  <Link href="#Projects" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Projects</Link>
+                  <Link href="#Contact" className='w-fit hover:bg-slate-50 py-2 px-5 rounded-full'> Contact</Link>
               </div>
               }
         </nav>
