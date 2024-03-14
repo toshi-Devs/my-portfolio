f8f3bdea87355bb2243581fc6eb3124cfc6d2ace
toshi-Devs <trahim87mustapha@gmail.com>
2024-03-14 13:20:09 +0200

    adding restaurant project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -12,7 +12,7 @@ function Projects() {
            
                     <div className='grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-2'>
 
-                    <div className='bg-[#1C1C1C] m-3 border-2 border-lime-100 rounded-sm shadow-lg backdrop-blur-md p-5'>
+                        <div className='bg-[#1C1C1C] m-3 border-2 border-lime-100 rounded-sm shadow-lg backdrop-blur-md p-5'>
                          {/* Now i will put a link to code and live website on top right of this div */}
                             <div className='flex flex-row justify-end space-x-3 pt-2'>
                                 <a href='https://github.com/toshi-Devs/my-portfolio' target='_blank' 
@@ -28,6 +28,29 @@ function Projects() {
                                     <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FBD38D] border-[#FBD38D] bg-[#fbd38d29]'> NEXTJS</p>
                                     <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#9DECF9] border-[#9DECF9] bg-[#fbd38d29]'> Typescript</p>
                                     <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FBB6CE] border-[#FBB6CE] bg-[#fbd38d29]'> TailwindCSS</p>
+                                </div>  
+                            </div>                          
+                        </div>
+
+                        <div className='bg-[#1C1C1C] m-3 border-2 border-lime-100 rounded-sm shadow-lg backdrop-blur-md p-5'>
+                         {/* Now i will put a link to code and live website on top right of this div */}
+                            <div className='flex flex-row justify-end space-x-3 pt-2'>
+                                <a href='https://github.com/toshi-Devs/my-portfolio' target='_blank' 
+                                className='text-slate-100 hover:text-[#40CF8E] transition-all duration-300 flex flex-col items-center'><FaGithub size={25} /></a>
+                                <a href='https://toshi-devs.vercel.app/' target='_blank' 
+                                className='text-slate-100 hover:text-[#40CF8E] transition-all duration-300 flex flex-col items-center'><FaLink size={25} /> </a>
+                            </div>
+
+                            <div className='flex flex-col items-center justify-center space-y-5 pb-5 cursor-pointer'>
+                                <h3 className='text-xl font-bold font-sans text-slate-100'> Restaurant Application</h3>
+                                <p className='text-lg text-slate-400 font-medium text-center'>A modern italian food restaurant application, where the user can check the menu and order the food, and login to see orders.</p>
+                                <div className='flex flex-row space-x-1 gap-1 items-center uppercase font-mono'>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FBD38D] border-[#FBD38D] bg-[#fbd38d29]'> NEXTJS</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#9DECF9] border-[#9DECF9] bg-[#fbd38d29]'> Typescript</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#D6BCFA] border-[#D6BCFA] bg-[#fbd38d29]'> Prisma</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FAF089] border-[#FAF089] bg-[#fbd38d29]'> PostgreSQL</p>
+                                    <p className='text-sm font-semibold px-1 border-2 rounded-ee-lg text-[#FBB6CE] border-[#FBB6CE] bg-[#fbd38d29]'> Responsive</p>
+
                                 </div>  
                             </div>                          
                         </div>
